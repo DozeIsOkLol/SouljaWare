3122f8bed86e8e58dafc008d8a8e3bf3b47a87fd
DOZE / UILib <afterlife.records@outlook.com>
2026-01-30 21:10:08 +0000

    Update and rename CreditsTab.lua to Credits.lua

---
+++ Modules/Credits.lua
@@ -0,0 +1,19 @@
+return function(CreditsTab)
+    CreditsTab:AddParagraph('Developers', 
+        'Script: SouljaWitchSrc\n'..
+        'Tester: Aspire\n'..
+        'Testers: Aspire\n'..
+        'Kill Aura and Teleports from Neutral (Ive updated them)\n'..
+        'Make sure to check his script out'
+    )
+    
+    CreditsTab:AddParagraph('Libraries & Assets', 
+        'UI Library: XanBar[](https://xan.bar)\n'..
+        'Icons: XanBar'
+    )
+    
+    CreditsTab:AddParagraph('Thanks For Using', 
+        'Thanks for supporting this script!\n'..
+        'Enjoy responsibly ~ Soulja-Ware'
+    )
+end
