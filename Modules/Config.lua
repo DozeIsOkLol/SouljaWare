fba552e6dc051eaf7143d1c7bb16d6ca3b0ffc13
DOZE / UILib <afterlife.records@outlook.com>
2026-01-30 21:11:35 +0000

    Create Config.lua

---
+++ Modules/Config.lua
@@ -0,0 +1,12 @@
+local Config = {}
+
+Config.TESTER_MODE    = false
+Config.TesterWhitelist = {158212026, 10041780182, 2990059374}
+
+Config.UI = {
+    Title    = "Soulja-Ware",
+    Subtitle = "LSRPG:R (Release)",   -- change to "(TESTER)" when tester mode on
+    Theme    = "Rose",
+}
+
+return Config
